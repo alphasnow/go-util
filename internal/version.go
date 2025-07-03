3c61f7b3e449b20a68b9c4052a8de0e342a69edc
weifeng <wind91@foxmail.com>
2025-07-03 14:58:36 +0800

    v2

--- internal/version.go
+++ internal/version.go
@@ -1,6 +1,6 @@
 package internal
 
-var version = "1.0.0"
+var version = "2.0.0"
 
 func Version() string {
 	return version
--- util.go
+++ util.go
@@ -1,6 +1,6 @@
 package util
 
-import "github.com/alphasnow/go-util/internal"
+import "github.com/alphasnow/go-util/v2/internal"
 
 func Version() string {
 	return internal.Version()
